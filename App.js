d6957f2432e426afac300b6d6d5f48c366e31669
Nosheen999 <nosheenazhar999@gmail.com>
2023-09-10 19:48:42 +0500

    First commit

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1}}>
+      <View style={{flex:0.30, backgroundColor:'white', justifyContent: "center", marginLeft: 50}}>
+
+      <Image style={{alignContent: "center"}} source = {require('./Logo.jpeg')}/>
+  
+      </View>
+
+      <View style={{flex:0.15, backgroundColor:'purple', justifyContent: "center"}}>
+      <Text style={{backgroundColor:'purple', color: 'white', fontSize: 30, textAlign: "center"}}>Login page</Text>
+      </View>
+      
+        <View style={{flex:0.25,justifyContent: "center", backgroundColor:'pink'}}>
+      <TextInput
+ backgroundColor= "white"
+ //lets check git
+/>  
+</View> 
+<View style={{flex:0.05, backgroundColor:'pink'}}> 
+<TextInput
+ backgroundColor= "white"
+/>
+
+      </View>
+      <View style={{flex:0.25, backgroundColor:'pink', justifyContent: "center"}}>
+      <Button
+  
+  title="Login"
+  color="#841584"
+/>
+      </View>
     </View>
   );
 }
